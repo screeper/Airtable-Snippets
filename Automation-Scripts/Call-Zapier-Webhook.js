d187df196b0207e00b1b8f18603bd54316ef3608
Sebastian Wehrli <sebastian.wehrli@gmail.com>
2023-02-23 13:34:14 +0100

    Removed URL

--- Automation-Scripts/Call-Zapier-Webhook.js
+++ Automation-Scripts/Call-Zapier-Webhook.js
@@ -4,8 +4,12 @@ GET containing record id to a Zapier Webhook
 Input Variable "record_id" has to be set in Airtable.
 */
 
-let zapier_url = 'https://hooks.zapier.com/hooks/catch/14607154/3o8lptl/?record_id=';
+// Config
+let zapier_url = 'https://hook-url';
+// Config End
+
+
 let record_id = input.config().record_id;
-zapier_url = zapier_url + record_id;
+zapier_url = zapier_url + '?record_id=' + record_id;
 let response = await fetch(zapier_url);
 console.log(await response.json());
